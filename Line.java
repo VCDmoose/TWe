c2ed6f91bbbd76d0532d50ee9fd426a041431763
VCD <varunacdev.mec@gmail.com>
2017-06-22 17:26:24 +0530

    Horizontal stars

---
+++ Line.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Line{
+
+	static void printStar(int number){
+
+			while(number>0){
+			System.out.print("*");
+			number--;
+		}
+
+	}
+
+	public static void main(String[] args){
+		
+		Scanner sc=new Scanner(System.in); 
+		int starNumber = sc.nextInt();
+		printStar(starNumber);
+		
+	}
+}
