acd768e8d82c4ccb88ec09ce1cb8ff6812fe349e
VCD <varunacdev.mec@gmail.com>
2017-06-22 17:42:01 +0530

    Right Triangle

---
+++ RTriangle.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class RTriangle{
+
+	static void printStar(int number){
+
+			while(number>0){
+			System.out.print("*");
+			number--;
+		}
+		System.out.println();
+
+	}
+
+	public static void main(String[] args){
+		
+		Scanner sc=new Scanner(System.in); 
+		int starNumber = sc.nextInt();
+		int i;
+		
+		for(i=1;i<=starNumber;i++)
+			printStar(i);
+		
+	}
+}
